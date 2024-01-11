47ab2a84914efb108b3c048999296ed27229b345
Yana160916 <144559656+Yana160916@users.noreply.github.com>
2024-01-12 00:25:01 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -3,7 +3,7 @@ const getTotalBalanceByGender = (users, gender) =>
     .filter(user => user.gender === gender)
     .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 
-const allUsers = [
+const allUser = [
   {
     name: "Moore Hensley",
     gender: "male",
@@ -41,6 +41,6 @@ const allUsers = [
   }
 ];
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUser, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUser, "female")); // 8863
